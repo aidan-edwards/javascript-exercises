9238190afc2c5f598e50cdbd4a3d781c6cef0f74
Aidan Edwards <114438321+aidan-edwards@users.noreply.github.com>
2023-01-24 20:38:07 -0500

    Create for loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
 const repeatString = function(string, num) {
+    //loop through the input string
+    for (let i = 0; i < string.length; i++){
 
+    }
 };
+
+//create arr to store output
+const myOutput = [];
 
 // Do not edit below this line
 module.exports = repeatString;
