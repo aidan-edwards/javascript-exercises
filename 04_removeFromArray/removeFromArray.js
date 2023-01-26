3f13f265dd52ee1d9dc167fd393fd3c83961b69a
Aidan Edwards <114438321+aidan-edwards@users.noreply.github.com>
2023-01-25 22:34:08 -0500

    Filter arr with args

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,7 +2,8 @@
 //and an arr
 //removes those args from that arr
 const removeFromArray = function(arr, ...args) {
-
+    const result = arr.filter(item => item != ...args);
+    return result;
 };
 
 // Do not edit below this line
