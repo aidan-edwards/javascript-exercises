6d723aa94d73cc85cb054bedc8529f1b63024275
Aidan Edwards <114438321+aidan-edwards@users.noreply.github.com>
2023-01-26 18:41:10 -0500

    Finish all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+//generate an array with both arguments and every number in between
+//use a loop and say IF less than max keep pushing i to the array after incrementing
+//use a reduce callback function to sum them all up
+//save that to a variable
+//return the variable
 
-};
+
+
+
+
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
+    if (min > max) {
+      const temp = min;
+      min = max;
+      max = temp;
+    }
+    let sum = 0;
+    for (let i = min; i < max + 1; i++) {
+      sum += i;
+    }
+    return sum;
+  };
 
 // Do not edit below this line
 module.exports = sumAll;
